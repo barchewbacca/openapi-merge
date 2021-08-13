405f4083164c94d66bb6b9d5a84bf33cef46a507
Ilia Barchugov <barchewbacca@gmail.com>
2021-08-13 20:29:11 +0200

    test out getting file from the workflow context

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,48 @@
 import * as core from '@actions/core';
+// import axios from 'axios';
+// import { execSync } from 'child_process';
+import fs from 'fs';
 
 async function run(): Promise<void> {
   try {
     const token = core.getInput('token', { required: true });
     const urls = core.getInput('urls', { required: true });
 
-    const urlList = JSON.parse(urls);
+    // const urlList = JSON.parse(urls);
 
+    // const json: any = {
+    //   inputs: [],
+    //   output: './output.swagger.json',
+    // };
+
+    const rawdata = fs.readFileSync('migrations.json', 'utf-8');
+    const migrations = JSON.parse(rawdata);
     // eslint-disable-next-line no-console
-    console.log('Test', urlList, token);
+    console.log(migrations, token, urls);
+
+    // fs.mkdirSync('openapi', { recursive: true });
+
+    // for (const [index, url] of urlList.entries()) {
+    //   const rawUrl = `https://raw.githubusercontent.com/${url.replace('https://github.com/', '').replace('blob/', '')}`;
+
+    //   const { data } = await axios.get(rawUrl, {
+    //     headers: {
+    //       Authorization: `Bearer ${token}`,
+    //       'Content-Type': 'application/json',
+    //       Accept: '*/*',
+    //     },
+    //   });
+
+    //   fs.writeFileSync(`openapi-${index}.yaml`, data);
+
+    //   json.inputs.push({
+    //     inputFile: `openapi-${index}.yaml`,
+    //   });
+    // }
+
+    // fs.writeFileSync('openapi-merge.json', JSON.stringify(json));
+
+    // execSync('npx openapi-merge-cli');
   } catch (error) {
     core.setFailed(error.message);
   }
